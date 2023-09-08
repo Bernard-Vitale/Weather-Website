696f0785a0a94aead2a0639d009e66b0baa0e50f
Bernard Vitale <126836061+Bernard-Vitale@users.noreply.github.com>
2023-09-08 00:48:48 -0400

    Updated app.js

--- weather_website/app.js
+++ weather_website/app.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(express.static(__dirname + "/public/"));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
 app.get('/api', async (request, response) => {
     const city = request.query.city;
     const apiResponse = await fetch(apiUrl + city + `&appid=${apiKey}`);
